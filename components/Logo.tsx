476689f8088c9016c810e59a87da0dd4ec66aa0d
Taufik-H <taufikrpl1@gmail.com>
2024-06-07 08:11:12 +0700

    under development main

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <div className="flex justify-center items-center flex-col w-fit">
       <div className="text-xs font-black text-pink-600">baturraden</div>
-      <div className={cn(londrina.className, "text-2xl font-black text-white")}>
+      <div className={cn(londrina.className, "text-2xl font-black")}>
         Trail Run
       </div>
     </div>
