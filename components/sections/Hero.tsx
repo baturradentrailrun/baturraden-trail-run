63a543b6263e35c731e043f772653d3916c1057b
Taufik-H <taufikrpl1@gmail.com>
2024-06-15 01:03:03 +0700

    button hero

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import React from "react";
 import HeroBackround from "../HeroBackround";
+import Link from "next/link";
 
 const Hero = ({ id }: { id: string }) => {
   return (
@@ -28,8 +29,49 @@ const Hero = ({ id }: { id: string }) => {
               <p className={cn(plusJakartaSans.className, "lowercase text-xs")}>
                 part of
               </p>
-              <p>baturaden</p>
+              <p>baturraden</p>
               <p className="text-3xl text-[#97DE00]">festival</p>
+              <p className={cn(plusJakartaSans.className, "text-sm mt-5")}>
+                Registrasi melalui aplikasi :
+              </p>
+              <div className="flex gap-2 text-white mt-5">
+                <Link
+                  href={
+                    "https://play.google.com/store/apps/details?id=asia.yoayo.yoayo&pcampaignid=web_share"
+                  }
+                  target="_blank"
+                  className="flex gap-2  bg-black px-5 py-2 rounded-xl border-4 border-slate-600"
+                >
+                  <Image
+                    src={"playstore.svg"}
+                    width={20}
+                    height={20}
+                    alt="playstore"
+                  />
+                  <div>
+                    <p className="text-sm">GET IT ON</p>
+                    <p className="font-bold">Google Play</p>
+                  </div>
+                </Link>
+                <Link
+                  href={
+                    "https://apps.apple.com/us/app/yoayo-active-community/id1481334742"
+                  }
+                  target="_blank"
+                  className="flex gap-4 text-white bg-black px-5 py-2 rounded-xl border-4 border-slate-600"
+                >
+                  <Image
+                    src={"appstore.svg"}
+                    width={20}
+                    height={20}
+                    alt="appstore"
+                  />
+                  <div>
+                    <p className="text-sm">GET IT ON</p>
+                    <p className="font-bold">App Store</p>
+                  </div>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
