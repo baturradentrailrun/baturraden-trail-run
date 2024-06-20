4e3155c427d213cce56273fd1cb278d4722992f0
Taufik-H <taufikrpl1@gmail.com>
2024-06-20 19:26:09 +0700

    improvement

--- components/HeroBackround.tsx
+++ components/HeroBackround.tsx
@@ -10,6 +10,7 @@ function HeroBackround() {
         width={30}
         height={30}
         className="absolute z-[2] w-full h-full"
+        priority
       />
       <Image
         src={"edge1.svg"}
@@ -17,6 +18,7 @@ function HeroBackround() {
         width={80}
         height={80}
         className="absolute z-[3] top-0 left-0 w-36 lg:w-60"
+        priority
       />
       <Image
         src={"edge2.svg"}
@@ -24,6 +26,7 @@ function HeroBackround() {
         width={80}
         height={80}
         className="absolute z-[3] top-5 -right-5 md:top-0 md:right-0 w-36 md:w-60 -rotate-[40deg] md:rotate-0"
+        priority
       />
       <Image
         src={"edge3.svg"}
@@ -31,6 +34,7 @@ function HeroBackround() {
         width={80}
         height={80}
         className="absolute z-[3] -bottom-3 right-0 w-36 lg:w-60"
+        priority
       />
       <Image
         src={"edge4.svg"}
@@ -38,6 +42,7 @@ function HeroBackround() {
         width={80}
         height={80}
         className="absolute z-[3] bottom-0 left-0 w-36 lg:w-60"
+        priority
       />
       <Image
         src={"center.svg"}
@@ -45,6 +50,7 @@ function HeroBackround() {
         width={100}
         height={100}
         className="absolute z-[2] w-screen h-screen scale-[4] lg:scale-[2.5]"
+        priority
       />
       <Image
         src={"fluid.svg"}
@@ -52,6 +58,7 @@ function HeroBackround() {
         width={30}
         height={30}
         className="absolute z-[2] w-full h-full scale-[1.4] md:scale-[1] rotate-90 lg:rotate-0 lg:scale-[1] lg:-translate-x-8 lg:translate-y-8"
+        priority
       />
 
       <Image
@@ -60,6 +67,7 @@ function HeroBackround() {
         width={30}
         height={30}
         className="absolute z-[2] w-full h-full scale-[1.4] md:scale-[1]  lg:scale-[0.4] translate-y-28"
+        priority
       />
     </>
   );
