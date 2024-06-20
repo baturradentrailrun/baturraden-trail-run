c02ebcc37098d6b1f853623875686a79bfcdcfa7
Taufik-H <taufikrpl1@gmail.com>
2024-06-20 19:56:26 +0700

    feat:rute

--- components/sections/Rute.tsx
+++ components/sections/Rute.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import { Card } from "../ui/card";
+import Image from "next/image";
 
 const Rute = ({ id }: { id: string }) => {
   return (
     <section id={id} className="bg-slate-900 relative  padding-container py-10">
-      <div className="flex flex-col justify-center items-center mb-5">
-        <div className="text-3xl font-black uppercase">rute</div>
-        <div className="capitalize text-slate-500">
-          rute baturraden trail run
-        </div>
+      <div className="p-5 bg-white bg-opacity-10 backdrop-blur-md rounded-2xl border-t border-slate-500 border-l ">
+        <Card className="w-full rounded-xl overflow-hidden ">
+          <iframe
+            src="https://www.google.com/maps/d/embed?mid=15reFvGVbgvmRQx3b2ZZ267c77V2zCNQ&ehbc=2E312F"
+            width="100%"
+            height="480"
+          ></iframe>
+        </Card>
       </div>
-      <Card className="w-full rounded-xl overflow-hidden shadow-xl boeder">
-        <iframe
-          src="https://www.google.com/maps/d/embed?mid=15reFvGVbgvmRQx3b2ZZ267c77V2zCNQ&ehbc=2E312F"
-          width="100%"
-          height="480"
-        ></iframe>
-      </Card>
     </section>
   );
 };
