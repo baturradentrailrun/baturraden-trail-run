bc9d28d30923c64d562f05d490cee4f566272f00
Taufik Hidayatulloh <44672713+Taufik-H@users.noreply.github.com>
2024-08-02 14:57:12 +0700

    Update fetchData.ts

--- sanity/fetch/fetchData.ts
+++ sanity/fetch/fetchData.ts
@@ -1,18 +1,13 @@
 import { groq } from "next-sanity";
 import { client } from "../lib/client";
 
-export async function fetchData({
-  query,
-  slug,
-}: {
-  query: string;
-  slug?: string;
-}) {
+export async function fetchData({ query, slug }: { query: string; slug?: string }) {
   try {
     const params = slug ? { slug } : {};
     const data = await client.fetch(groq`${query}`, params);
     return data;
   } catch (error: any) {
+    console.error("Error fetching data:", error);
     throw new Error(`Error fetching data: ${error.message}`);
   }
 }
